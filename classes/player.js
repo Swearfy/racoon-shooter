137c530198c6120275c228f932f0ec6b44b1eb6b
Swearfy <swearfy@gmail.com>
2022-11-21 13:32:03 +0000

    player class basic movement

--- classes/player.js
+++ classes/player.js
@@ -1,3 +1,19 @@
 export class Player {
-  constructor() {}
+  constructor(game) {
+    this.game = game;
+    this.x = 50;
+    // this.game.width / 2 - 15;
+    this.y = 50;
+  }
+  update() {}
+  draw(ctx) {
+    ctx.fillStyle = "black";
+    ctx.fillRect(this.x, this.y, 30, 30);
+  }
+  moveX(dir) {
+    this.x += dir;
+  }
+  moveY(dir) {
+    this.y += dir;
+  }
 }
