2f705315f5424fb9eefd3f721a1d5e190bfaffac
Swearfy <swearfy@gmail.com>
2023-01-27 15:06:29 +0000

    Moved player movment in player.update

--- classes/player.js
+++ classes/player.js
@@ -1,3 +1,4 @@
+import { Input } from "./input.js";
 export class Player {
   constructor(game, x, y) {
     this.game = game;
@@ -5,19 +6,40 @@ export class Player {
     this.y = y;
     this.speedX = 0;
     this.speedY = 0;
+    this.maxSpeed = 0;
+    this.input = new Input(this.game);
   }
   update(gameSpeed, frameTimeDelta) {
+    this.maxSpeed = 2;
     this.x += this.speedX * gameSpeed * frameTimeDelta;
     this.y += this.speedY * gameSpeed * frameTimeDelta;
+
+    if (
+      (this.input.p1Keys.w.pressed || this.input.p1Keys.s.pressed) &&
+      (this.input.p1Keys.a.pressed || this.input.p1Keys.d.pressed)
+    ) {
+      this.maxSpeed = this.maxSpeed * 0.71;
+    }
+
+    if (this.input.p1Keys.w.pressed) {
+      this.speedY = -this.maxSpeed;
+    } else if (this.input.p1Keys.s.pressed) {
+      this.speedY = this.maxSpeed;
+    } else {
+      this.speedY = 0;
+    }
+
+    if (this.input.p1Keys.a.pressed) {
+      this.speedX = -this.maxSpeed;
+    } else if (this.input.p1Keys.d.pressed) {
+      this.speedX = this.maxSpeed;
+    } else {
+      this.speedX = 0;
+    }
+    console.log(this.maxSpeed);
   }
   draw(ctx) {
     ctx.fillStyle = "black";
     ctx.fillRect(this.x, this.y, 30, 30);
-  }
-  moveX(x) {
-    this.speedX = x;
-  }
-  moveY(y) {
-    this.speedY = y;
   }
 }
--- main.js
+++ main.js
@@ -1,4 +1,3 @@
-import { Input } from "./classes/input.js";
 import { Player } from "./classes/player.js";
 
 const canvas = document.querySelector("canvas");
@@ -14,39 +13,12 @@ class Game {
     this.width = width;
     this.height = height;
     this.player = new Player(this, canvas.width / 2, canvas.height / 2);
-    this.input = new Input(this);
   }
   update(gameSpeed, frameTimeDelta) {
     this.player.update(gameSpeed, frameTimeDelta);
   }
   draw(ctx) {
     this.player.draw(ctx);
-  }
-  movment() {
-    //player 1
-    let maxSpeed = 2;
-
-    if (
-      (this.input.p1Keys.w.pressed || this.input.p1Keys.s.pressed) &&
-      (this.input.p1Keys.a.pressed || this.input.p1Keys.d.pressed)
-    ) {
-      maxSpeed *= 0.71;
-    }
-    if (this.input.p1Keys.w.pressed) {
-      this.player.moveY(-maxSpeed);
-    } else if (this.input.p1Keys.s.pressed) {
-      this.player.moveY(maxSpeed);
-    } else {
-      this.player.moveY(0);
-    }
-
-    if (this.input.p1Keys.a.pressed) {
-      this.player.moveX(-maxSpeed);
-    } else if (this.input.p1Keys.d.pressed) {
-      this.player.moveX(maxSpeed);
-    } else {
-      this.player.moveX(0);
-    }
   }
 }
 
@@ -66,7 +38,6 @@ function animate(currentTime) {
   ctx.fillStyle = "wheat";
   ctx.fillRect(0, 0, canvas.width, canvas.height);
 
-  game.movment();
   game.update(gameSpeed, frameTimeDelta);
   game.draw(ctx);
 
