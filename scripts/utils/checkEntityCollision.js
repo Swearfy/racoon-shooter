6eee2bf2451b4ffea5ae35b391d8b13334ec176c
Swearfy <swearfy@gmail.com>
2023-04-20 09:20:13 +0100

    entity collision ?

--- scripts/utils/checkEntityCollision.js
+++ scripts/utils/checkEntityCollision.js
@@ -1,1 +1,11 @@
-export function checkObjectCollision(object1, object2) {}
+export function checkObjectCollision(gameObject1, gameObject2) {
+  if (
+    gameObject2.x > gameObject1.width + gameObject1.x ||
+    gameObject1.x > gameObject2.width + gameObject2.x ||
+    gameObject2.y > gameObject1.height + gameObject1.y ||
+    gameObject1.y > gameObject2.height + gameObject2.y
+  ) {
+    return false;
+  }
+  return true;
+}
